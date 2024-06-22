6b1efce799001c561e3c41f2c2bcd3e29ec1d034
muhammadfariz123 <131078732+muhammadfariz123@users.noreply.github.com>
2024-06-22 14:56:46 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "WarungBakso";
+$db_name = "mysql:host=db;dbname=warungbakso";
+$user_name = "warungbakso";
+$user_password = "warungbakso";
 
-$koneksi = new mysqli($servername, $username, $password, $dbname);
+$koneksi = new mysqli($db_name, $user_name, $user_password);
+
 
 if ($koneksi->connect_error) {
     die("Koneksi gagal: " . $koneksi->connect_error);
