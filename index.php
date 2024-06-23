b4eeb549dfb5f7eff90289755449400534c4a811
muhammadfariz123 <131078732+muhammadfariz123@users.noreply.github.com>
2024-06-23 09:03:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 header('location: beranda');
+
+// 1. Afif Ramadhani (2211104066)
+// 2. Muhammad Rizaldy Akbar (2211104065)
+// 3. Muhammad Fariz Nur Hidayat (2211104069)
+// 4. Dzikri Naufal Wisnu Pravida (2211104063)
+// 5. Kholil Abdi Prasetyo (2211104071)
+
+
